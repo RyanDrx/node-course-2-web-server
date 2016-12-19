63aed76097f1d904c9bf3ad682918f4ad82e1d48
Ryand <Ryand@hptest.info>
2016-12-19 11:53:51 -0800

    Setup Start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -28,8 +29,8 @@ app.use((req, res, next) => {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`)
 });
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -59,8 +60,10 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/bad', (req, res) => {
+
     res.send({
         errorMessage: 'Unable to fulfill request!'
     });
+
 });
 
